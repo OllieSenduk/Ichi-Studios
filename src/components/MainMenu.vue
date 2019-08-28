de1b326e4b92bafa1a468f70aaca8bfe243c5492
Ollie <ollie_senduk@hotmail.com>
2019-08-28 21:23:55 +0200

    FEATURE - Makes sure that both transition leaving page and entering page works with Gridsome framework

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class='name'>
+        <g-link to="/about">About us</g-link>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang='sass'>
+</style>
