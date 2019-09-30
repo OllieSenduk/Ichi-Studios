d989fc1d9c6326eed252f9dc09386313b2131069
Ollie <ollie_senduk@hotmail.com>
2019-09-30 16:07:51 +0200

    temp commit

--- src/components/projects/ProjectPage.vue
+++ src/components/projects/ProjectPage.vue
@@ -1,202 +1,211 @@
 <template>
-    <section class="project__wrapper">
-        <div class='project' :class='{project__hidden: hidden}'>
-            <div class="project__title">{{title}}</div>
-            <div class="project__details">
-                <div class="project__small-title">{{title}} </div>
-                <div class="project__description">{{text}}</div>
-            </div>
-            <div class="project__hero">
-                <a href="">
-                    <img class="project__hero__image-left" :src="imgLeft" :alt="title" :class="{ project__hero__disappear: navStatus }">
-                    <img class="project__hero__image-right" :src="imgRight" :alt="title" :class="{ project__hero__disappear: navStatus }">
-                </a>
-            </div>
-
-        </div>
-    </section>
+  <section class="project__wrapper">
+    <div class="project" :class="{project__hidden: hidden}">
+      <div class="project__title">{{title}}</div>
+      <div class="project__details">
+        <div class="project__small-title">{{title}}</div>
+        <div class="project__description">{{text}}</div>
+      </div>
+      <div class="project__hero">
+        <a href>
+          <img
+            class="project__hero__image-left"
+            :src="imgLeft"
+            :alt="title"
+            :class="{ project__hero__disappear: navStatus }"
+          />
+          <img
+            class="project__hero__image-right"
+            :src="imgRight"
+            :alt="title"
+            :class="{ project__hero__disappear: navStatus }"
+          />
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    import  { mapState, mapGetters, mapMutations } from 'vuex'
+import { mapState, mapGetters, mapMutations } from "vuex";
 
-    export default {
-    props: ['id', 'title', 'imgRight', 'imgLeft', 'text', 'categories', 'background', 'hidden', 'pageNum'],
-        data() {
-            return {
-            }
-        },
-        computed: {
-            ...mapState([
-                'navOpen'
-            ]),
-            ...mapGetters([
-                'navStatus'
-            ]),
-        },
-        // methods: {
-            // changeTitleSize() {
-            //     const context = this
-            //     this.$nextTick(() => { 
-            //         const titles = document.querySelectorAll('.project__title')
-            //         titles.forEach(function(title) {
-            //             console.log(context.title.length)
-            //             if (context.title.length >= 12) {
-            //                 title.style.fontSize = `calc(3vw + 6vh + -4vmin)`
-            //             }
-            //             if (context.title.length < 12 ) {
-            //                 title.style.fontSize = `calc(4vw + 8vh + -4vmin)`
-            //             } 
+export default {
+  props: [
+    "id",
+    "title",
+    "imgRight",
+    "imgLeft",
+    "text",
+    "categories",
+    "background",
+    "hidden",
+    "pageNum"
+  ],
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["navOpen"]),
+    ...mapGetters(["navStatus"])
+  },
+  // methods: {
+  // changeTitleSize() {
+  //     const context = this
+  //     this.$nextTick(() => {
+  //         const titles = document.querySelectorAll('.project__title')
+  //         titles.forEach(function(title) {
+  //             console.log(context.title.length)
+  //             if (context.title.length >= 12) {
+  //                 title.style.fontSize = `calc(3vw + 6vh + -4vmin)`
+  //             }
+  //             if (context.title.length < 12 ) {
+  //                 title.style.fontSize = `calc(4vw + 8vh + -4vmin)`
+  //             }
 
-                        
-                        // if context.title.length {
-                        //     title.style.fontSize = `calc(3vw + 12vh + -4vmin)`
+  // if context.title.length {
+  //     title.style.fontSize = `calc(3vw + 12vh + -4vmin)`
 
-                        // }
-                //     })
-                //  })
+  // }
+  //     })
+  //  })
 
-           
-            //    titles.forEach( 
-            //     function(currentValue, currentIndex, listObj) { 
-            //         console.log(currentValue + ', ' + currentIndex + ', ' + this); 
-            //     },
-            //     'myThisArg'
-            //     );
-        //     }
-        // },
-        // created() {
-        //     if (window.innerWidth > 740) {
-        //             this.changeTitleSize()
-        //      }
-        // },
-            // this.$store.watch(
-            //     (state, getters) => getters.navStatus,
-            //     (updated, old) => {
-            //         console.log(old)
-            //     }
-            // )
-        watch: {
-            navOpen(newVal, oldVal) {
-            }
-        }
-    }
+  //    titles.forEach(
+  //     function(currentValue, currentIndex, listObj) {
+  //         console.log(currentValue + ', ' + currentIndex + ', ' + this);
+  //     },
+  //     'myThisArg'
+  //     );
+  //     }
+  // },
+  // created() {
+  //     if (window.innerWidth > 740) {
+  //             this.changeTitleSize()
+  //      }
+  // },
+  // this.$store.watch(
+  //     (state, getters) => getters.navStatus,
+  //     (updated, old) => {
+  //         console.log(old)
+  //     }
+  // )
+  watch: {
+    navOpen(newVal, oldVal) {}
+  }
+};
 </script>
 
 <style lang='scss' scoped>
+.project {
+  font-family: $main-font;
+  font-weight: $thin-text;
+  color: white;
 
-    .project {
-        font-family: $main-font;
-        font-weight: $thin-text;
-        color: white;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 5% 1fr 5%;
+  grid-template-rows: 20% 3fr 1fr 10% 10%;
+  align-items: center;
+  @include mq($from: tablet) {
+    grid-template-columns: 15% 1fr 15%;
+  }
 
-        max-height: 100vh;
-        display: grid;
-        grid-template-columns: 5% 1fr 5%;
-        grid-template-rows: 20% 3fr 1fr 10% 10%;
-        align-items: center;
-        @include mq($from: tablet) {
-            grid-template-columns: 15% 1fr 15%;
-        }
+  &__hidden {
+    position: absolute;
+    bottom: 0%;
+    left: 0%;
+    width: 100%;
+    opacity: 0;
+    pointer-events: none;
+  }
 
-        &__hidden {
-            position: absolute;
-            bottom: 0%;
-            left: 0%;
-            width: 100%;
-            opacity: 0;
-            pointer-events: none;
-        }
-    
-        &__hero {
-            overflow: hidden;
-            grid-column: 2/3;
-            grid-row: 2/3;
-            height: auto;
-            align-self: center;
-            justify-self: center;
-            display: flex;
+  &__hero {
+    overflow: hidden;
+    grid-column: 2/3;
+    grid-row: 2/3;
+    height: auto;
+    align-self: center;
+    justify-self: center;
+    display: flex;
 
-            img {
-                height: 380px;
-                transition: transform 0.3s ease-out;
-                cursor: pointer;
-            }
-
-            a {
-                display: flex;
-            }
-
-            &__image-right {
-                transform: translate(0%, 10%);
-            }
-
-            &__image-left {
-                transform: translate(0%, -10%);
-            }
-
-            &:hover &__image-right {
-                transform: translate(0%, 0%);
-            }
-            
-            &:hover &__image-left {
-                transform: translate(0%, 0%)
-            }
-
-            &__disappear {
-                // transform: translate(0%, -100%);
-            }
-        }
-
-        &__details {
-            grid-column: 2/3;
-            align-self: end;
-            grid-row: 3/4;
-            text-align: center;
-
-            @include mq($from: tablet) {
-                grid-row: 4/5;
-            }
-        }
-
-        &__title {
-            font-size: 2.5rem;
-            font-weight: $bold-text;
-            grid-row: 4/5;
-            grid-column: 2/3;
-            text-align: center;
-            @include mq($from: tablet) {
-                grid-row: 1/6;
-                grid-column: 1/2;
-                opacity: 0.2;
-                text-transform: uppercase;
-                writing-mode: vertical-rl;
-                align-self: center;
-                font-size: calc(3vw + 6vh + -4vmin);   
-            }
-        }
-
-        &__small-title {
-            display: none;
-
-            @include mq($from: tablet) {
-                display: block;
-                font-size: 2rem;
-                font-weight: bolder;
-                // margin-bottom: 20px;
-            }
-        }
-
-        &__description {
-            font-size: 2.4rem;
-            margin-bottom: 8px;
-            font-style: italic;
-
-            @include mq($from: tablet) {
-                font-size: 3.5rem;
-                font-style: normal;
-            }
-        }
-         
+    img {
+      height: 380px;
+      transition: transform 0.3s ease-out;
+      cursor: pointer;
     }
+
+    a {
+      display: flex;
+    }
+
+    &__image-right {
+      transform: translate(0%, 10%);
+    }
+
+    &__image-left {
+      transform: translate(0%, -10%);
+    }
+
+    &:hover &__image-right {
+      transform: translate(0%, 0%);
+    }
+
+    &:hover &__image-left {
+      transform: translate(0%, 0%);
+    }
+
+    &__disappear {
+      // transform: translate(0%, -100%);
+    }
+  }
+
+  &__details {
+    grid-column: 2/3;
+    align-self: end;
+    grid-row: 3/4;
+    text-align: center;
+
+    @include mq($from: tablet) {
+      grid-row: 4/5;
+    }
+  }
+
+  &__title {
+    font-size: 2.5rem;
+    font-weight: $bold-text;
+    grid-row: 4/5;
+    grid-column: 2/3;
+    text-align: center;
+    @include mq($from: tablet) {
+      grid-row: 1/6;
+      grid-column: 1/2;
+      opacity: 0.2;
+      text-transform: uppercase;
+      writing-mode: vertical-rl;
+      align-self: center;
+      font-size: calc(3vw + 6vh + -4vmin);
+    }
+  }
+
+  &__small-title {
+    display: none;
+
+    @include mq($from: tablet) {
+      display: block;
+      font-size: 2rem;
+      font-weight: bolder;
+      // margin-bottom: 20px;
+    }
+  }
+
+  &__description {
+    font-size: 2.4rem;
+    margin-bottom: 8px;
+    font-style: italic;
+
+    @include mq($from: tablet) {
+      font-size: 3.5rem;
+      font-style: normal;
+    }
+  }
+}
 </style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,18 +1,18 @@
 <template>
-        <section class='projects' :class="{ remove_page: navStatus }">
-        <appProjectPage 
-            v-for="project in projects" 
-            :key="project.id"
-            :title="project.title" 
-            :imgLeft="project.imgLeft"
-            :imgRight="project.imgRight"
-            :text="project.text"
-            :categories="project.categories"
-            :background="project.background"
-            :hidden="project.hidden"
-            :pageNum="project.pageNum"
-        ></appProjectPage>
-            <!-- <div class="projects__slider">
+  <section class="projects" :class="{ remove_page: navStatus }">
+    <appProjectPage
+      v-for="project in getArray"
+      :key="project.identifier"
+      :title="project.title"
+      :imgLeft="project.imgLeft"
+      :imgRight="project.imgRight"
+      :text="project.text"
+      :categories="project.categories"
+      :background="project.background"
+      :hidden="project.hidden"
+      :pageNum="project.pageNum"
+    ></appProjectPage>
+    <!-- <div class="projects__slider">
                 <div class="page-1">
 
                     <svg
@@ -73,188 +73,201 @@
                 >
                     <circle cx="6" cy="6" r="6" fill="white" />
                 </svg>
-            </div> -->
-          </div>
-    </section>
+    </div>-->
+  </section>
 </template>
 
 <script>
-import ProjectPage from '~/components/projects/ProjectPage'
-import  { mapState, mapGetters, mapMutations } from 'vuex'
-import { TweenMax } from 'gsap'
-import { TimelineMax } from 'gsap'
+import ProjectPage from "~/components/projects/ProjectPage";
+import { mapState, mapGetters, mapMutations } from "vuex";
+import { TweenMax } from "gsap";
+import { TimelineMax } from "gsap";
 
 export default {
-    computed: {
-        ...mapState([
-            'navOpen'
-        ]),
-        ...mapGetters([
-            'navStatus'
-        ])
+  computed: {
+    getArray() {
+      this.projects.slide(1);
+      // console.log(this.projects.slice(1, this.projects.length));
+      // return this.projects.slice(1, this.projects.length);
     },
-    components: {
-        appProjectPage: ProjectPage
+    ...mapState(["navOpen"]),
+    ...mapGetters(["navStatus"])
+  },
+  components: {
+    appProjectPage: ProjectPage
+  },
+  methods: {
+    updateSlide() {
+      //  console.log(this.pageNum)
+
+      const pages = this.$children;
+      // Current & Next Page
+      const currentPage = this.$children[this.current];
+      // const nextPage = this.$children[this.pageNum + 1];
+      const nextPage = this.$children[this.pageNum];
+      //    if (this.pageNum === this.$children.length-1) {
+      //         nextPage = this.$children[0];
+      //     } else {
+      //         nextPage = this.$children[this.pageNum + 1];
+      //     }
+      var context = this;
+      // Page Elements
+      // console.log(currentPage)
+      // console.log(nextPage)
+
+      const pagesContainer = document.querySelector(".projects");
+      const currentPageContainer = currentPage.$el.querySelector(".project");
+      const nextPageContainer = nextPage.$el.querySelector(".project");
+      const currentLeft = currentPage.$el.querySelector(
+        ".project__hero__image-left"
+      );
+      const currentRight = currentPage.$el.querySelector(
+        ".project__hero__image-right"
+      );
+      const currentText = currentPage.$el.querySelector(".project__details");
+
+      const nextLeft = nextPage.$el.querySelector(".project__hero__image-left");
+      const nextRight = nextPage.$el.querySelector(
+        ".project__hero__image-right"
+      );
+
+      const nextText = nextPage.$el.querySelector(".project__details");
+      // console.log(currentLeft)
+
+      const tl = new TimelineMax({});
+      tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" });
+      tl.fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2")
+        .to(pagesContainer, 0.3, {
+          backgroundImage: context.projects[context.pageNum].background
+        })
+        .fromTo(currentPageContainer, 0.3, { opacity: 1 }, { opacity: 0 })
+        .fromTo(
+          nextPageContainer,
+          0.3,
+          { opacity: 0, pointerEvents: "none" },
+          { opacity: 1, pointerEvents: "all" },
+          "-=0.6"
+        );
+      tl.fromTo(nextLeft, 0.3, { y: "-100%" }, { y: "-10%" }, "-=0.6")
+        .fromTo(nextRight, 0.3, { y: "-100%" }, { y: "10%" }, "-=0.8")
+        .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
+        .set(nextLeft, { clearProps: "all" })
+        .set(nextRight, { clearProps: "all" });
+
+      this.current = this.pageNum;
     },
-    methods: {
-        updateSlide() {
-        //  console.log(this.pageNum)
-
-            const pages = this.$children
-            // Current & Next Page
-            const currentPage = this.$children[this.current];
-            // const nextPage = this.$children[this.pageNum + 1];
-            const nextPage = this.$children[this.pageNum]
-        //    if (this.pageNum === this.$children.length-1) {
-        //         nextPage = this.$children[0];
-        //     } else {
-        //         nextPage = this.$children[this.pageNum + 1];
-        //     }
-            var context = this
-            // Page Elements
-            // console.log(currentPage)
-            // console.log(nextPage)
-
-            const pagesContainer = document.querySelector(".projects")
-            const currentPageContainer = currentPage.$el.querySelector(".project");
-            const nextPageContainer = nextPage.$el.querySelector('.project')
-            const currentLeft = currentPage.$el.querySelector(".project__hero__image-left");
-            const currentRight = currentPage.$el.querySelector(".project__hero__image-right");
-            const currentText = currentPage.$el.querySelector(".project__details");
-
-            const nextLeft = nextPage.$el.querySelector(".project__hero__image-left");
-            const nextRight = nextPage.$el.querySelector(".project__hero__image-right");
-
-            const nextText = nextPage.$el.querySelector(".project__details");
-            // console.log(currentLeft)
-
-            const tl = new TimelineMax({})
-            tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
-            tl.fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2")
-            .to(pagesContainer, 0.3, { backgroundImage: context.projects[context.pageNum].background })
-            .fromTo(currentPageContainer, 0.3, { opacity: 1 }, { opacity: 0 })
-            .fromTo(
-                nextPageContainer,
-                0.3,
-                { opacity: 0, pointerEvents: "none" },
-                { opacity: 1, pointerEvents: "all" },
-                "-=0.6"
-            )
-            tl.fromTo(nextLeft, 0.3, { y: "-100%" }, { y: "-10%" }, "-=0.6")
-            .fromTo(nextRight, 0.3, { y: "-100%" }, { y: "10%" }, "-=0.8")
-            .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
-            .set(nextLeft, { clearProps: "all" })
-            .set(nextRight, { clearProps: "all" });
-
-            this.current = this.pageNum
-
-
-        },
-        updateSlideNum(e) {
-             if (e.deltaY > 0) {
-                this.pageNum += 1;
-            } else {
-                this.pageNum -= 1;
-            }
-            if (this.pageNum === this.projects.length) {
-                this.pageNum = 0;
-            }
-            if (this.pageNum < 0) {
-                this.pageNum = 2;
-            }
-            this.updateSlide()
-        },
-        throttle(func, limit) {
-            let inThrottle;
-            return function() {
-                const args = arguments;
-                const context = this;
-                if (!inThrottle) {
-                func.apply(context, args);
-                inThrottle = true;
-                setTimeout(() => (inThrottle = false), limit);
-                }
-            };
-         }
+    updateSlideNum(e) {
+      if (e.deltaY > 0) {
+        this.pageNum += 1;
+      } else {
+        this.pageNum -= 1;
+      }
+      if (this.pageNum === this.projects.length) {
+        this.pageNum = 0;
+      }
+      if (this.pageNum < 0) {
+        this.pageNum = 2;
+      }
+      this.updateSlide();
     },
-    mounted () {
-        window.addEventListener("wheel", this.throttle(this.updateSlideNum, 1500));
-        window.addEventListener("touchmove", this.throttle(this.updateSlideNum, 1500));
-    },
-    destroyed () {
-        window.removeEventListener("wheel", this.throttle(this.updateSlideNum, 1500));
-        window.removeEventListener("touch", this.throttle(this.updateSlideNum, 1500));
-    },
-    data () {
-        return {
-            tl: new TimelineMax({}),
-            started: false,
-            scrollSlide: 0,
-            current: 0,
-            pageNum: 0,
-            projects: [
-                {   pageNum: 0,
-                    id: 'ikea',
-                    title: 'IKEA',
-                    imgLeft: 'https://i.imgur.com/aIsAVTR.jpg',
-                    imgRight: 'https://i.imgur.com/zGKi8MQ.png',
-                    url: '/ikea',
-                    text: 'The Wonderful Everyday',
-                    categories: ['Tech', 'Development', 'Project Management'],
-                    background: `radial-gradient(#2B3760, #0B1023)`,
-                    hidden: false
-                },
-                {
-                    pageNum: 1,
-                    id: 'creativos_de_la_noche',
-                    title: 'Creativos de la Noche',
-                    imgLeft: 'https://i.imgur.com/ieEjiHi.jpg',
-                    imgRight: 'https://i.imgur.com/AG1ezvi.jpg',
-                    url: '/creativos-de-la-noche',
-                    text: 'Campaign for Creativity',
-                    categories: ['Event', 'Production', 'Photography', 'Film'],
-                    background: `radial-gradient(#4E3022, #161616)`,
-                    hidden: true
-                },
-                {
-                    pageNum: 2,
-                    id: 'Super Cool',
-                    title: 'Super cool',
-                    imgLeft: 'https://i.imgur.com/ieEjiHi.jpg',
-                    imgRight: 'https://i.imgur.com/AG1ezvi.jpg',
-                    url: '/creativos-de-la-noche',
-                    text: 'Amazing',
-                    categories: ['Event', 'Production', 'Photography', 'Film'],
-                    background: `radial-gradient(#4E4342, #161616)`,
-                    hidden: true
-                },
-            ]
+    throttle(func, limit) {
+      let inThrottle;
+      return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+          func.apply(context, args);
+          inThrottle = true;
+          setTimeout(() => (inThrottle = false), limit);
         }
-    },
-}
+      };
+    }
+  },
+  mounted() {
+    window.addEventListener("wheel", this.throttle(this.updateSlideNum, 1500));
+    window.addEventListener(
+      "touchmove",
+      this.throttle(this.updateSlideNum, 1500)
+    );
+  },
+  destroyed() {
+    window.removeEventListener(
+      "wheel",
+      this.throttle(this.updateSlideNum, 1500)
+    );
+    window.removeEventListener(
+      "touch",
+      this.throttle(this.updateSlideNum, 1500)
+    );
+  },
+  data() {
+    return {
+      tl: new TimelineMax({}),
+      started: false,
+      scrollSlide: 0,
+      current: 0,
+      pageNum: 0,
+      projects: [
+        {
+          pageNum: 0,
+          identifier: "ikea",
+          title: "IKEA",
+          imgLeft: "https://i.imgur.com/aIsAVTR.jpg",
+          imgRight: "https://i.imgur.com/zGKi8MQ.png",
+          url: "/ikea",
+          text: "The Wonderful Everyday",
+          categories: ["Tech", "Development", "Project Management"],
+          background: `radial-gradient(#2B3760, #0B1023)`,
+          hidden: false
+        },
+        {
+          pageNum: 1,
+          identifier: "creativos_de_la_noche",
+          title: "Creativos de la Noche",
+          imgLeft: "https://i.imgur.com/ieEjiHi.jpg",
+          imgRight: "https://i.imgur.com/AG1ezvi.jpg",
+          url: "/creativos-de-la-noche",
+          text: "Campaign for Creativity",
+          categories: ["Event", "Production", "Photography", "Film"],
+          background: `radial-gradient(#4E3022, #161616)`,
+          hidden: true
+        },
+        {
+          pageNum: 2,
+          identifier: "Super Cool",
+          title: "Super cool",
+          imgLeft: "https://i.imgur.com/ieEjiHi.jpg",
+          imgRight: "https://i.imgur.com/AG1ezvi.jpg",
+          url: "/creativos-de-la-noche",
+          text: "Amazing",
+          categories: ["Event", "Production", "Photography", "Film"],
+          background: `radial-gradient(#4E4342, #161616)`,
+          hidden: true
+        }
+      ]
+    };
+  }
+};
 </script>
 <style lang='scss' scoped>
+.projects {
+  overflow-x: hidden;
+  background: radial-gradient(rgb(177, 139, 179), rgba(11, 16, 35, 1));
 
-    .projects {
-        overflow-x: hidden;
-        background: radial-gradient(rgb(177, 139, 179), rgba(11, 16, 35, 1));
+  &__slider {
+    position: absolute;
+    right: 5%;
+    top: 50%;
+    transform: translateY(50%);
 
-        &__slider {
-            position: absolute;
-            right: 5%;
-            top: 50%;
-            transform: translateY(50%);
-
-            > div {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                padding-bottom: 20px;
-            }
-        }
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-bottom: 20px;
     }
-
-
+  }
+}
 </style>
 
 
